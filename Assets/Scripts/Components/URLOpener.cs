5d84dbdb84df7bd4e499ef0247fd7996560579ba
idarous <idarous@outlook.com>
2020-03-03 00:31:11 +0000

    FYP Completed

---
+++ Assets/Scripts/Components/URLOpener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLOpener : MonoBehaviour
+{
+    [SerializeField] private string url;
+
+    public void Open()
+    {
+        Application.OpenURL(url);
+    }
+
+}
